9a5eb9e8b7fde82859be3b466c9626875a4d69eb
farhanabrar <79356576+farhanabrar@users.noreply.github.com>
2022-07-09 01:19:52 +0700

    Create block-number.js

---
+++ UAS/LESSON 6/hardhat-simple-store/tasks/block-number.js
@@ -0,0 +1,10 @@
+const { task } =require("hardhat/config")
+
+task("block-number", "print current block number").setAction(
+    async(taskArgs, hre) => {
+       const blockNumber = await hre.ethers.provider.getBlockNumber()
+       console.log(`Block Number : ${blockNumber}`)
+    }
+)
+
+module.exports = {}
